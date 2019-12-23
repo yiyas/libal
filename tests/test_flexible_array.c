23a0a36eef9d35481de9516a8999b19e0a6c0acd
fanchanghu <86736264@qq.com>
2019-12-23 23:58:49 +0800

    add license for all files

--- tests/test_flexible_array.c
+++ tests/test_flexible_array.c
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2015 - 2017 YIYAS
+ *
+ * This source code is licensed under BSD 3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/BSD-3-Clause
+ */
 #include <stddef.h>
 
 #include "../src/flexible_array.h"
